3f9909f0a988f6e2d69345667e9568e1868103f5
Mike Zigberman <mikhail.manager@mail.ru>
2022-02-17 01:59:35 +0100

    add login & logout system

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5+$=dpj44zg0a7^ycug^wz)7t5gl*yw5kmt-=(^jo$ac7)k+0e'
+SECRET_KEY = 'aaa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
